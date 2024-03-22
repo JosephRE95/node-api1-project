2218b3ef7ec5b0d66707064456fb1fdfd4ae9c64
Joseph Ellis <josephellis95@gmail.com>
2024-03-22 11:37:33 -0500

    post created

--- api/server.js
+++ api/server.js
@@ -3,6 +3,31 @@
 const express = require("express");
 const User = require("./users/model");
 const server = express();
+server.use(express.json());
+
+// create
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    });
+  } else {
+    User.insert(user)
+    .then(createdUser => {
+        res.status(201).json(createdUser)
+    })
+  }
+});
+
+// update 
+
+server.put('/hobbits/:id', (req, res) => { // PUT EXISTING HOBBIT
+    // the id to update is in `req.params.id` and the desired name in `req.body.name`
+    hobbits = hobbits.map(hob => hob.id == req.params.id
+      ? { ...hob, name: req.body.name } : hob);
+    res.status(200).json(hobbits);
+  });
 
 //find all
 
@@ -22,11 +47,10 @@ server.get("/api/users", (req, res) => {
 server.get("/api/users/:id", (req, res) => {
   User.findById(req.params.id)
     .then((user) => {
-      
-      if (!user){
+      if (!user) {
         res.status(404).json({
-            message: "The user with the specified ID does not exist", 
-        })
+          message: "The user with the specified ID does not exist",
+        });
       }
       res.status(200).json(user);
     })
@@ -37,6 +61,7 @@ server.get("/api/users/:id", (req, res) => {
       });
     });
 });
+
 
 
 
