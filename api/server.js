a6fedd23bdb8fed1ae1cb9cafd36fb73c28483b8
Joseph Ellis <josephellis95@gmail.com>
2024-03-22 10:40:02 -0500

    get request made

--- api/server.js
+++ api/server.js
@@ -1,14 +1,41 @@
 // BUILD YOUR SERVER HERE
 
-const express = require('express');
+const express = require("express");
+const User = require("./users/model");
+const server = express();
 
-const server = express(); 
+//find all
 
-server.use('*', (req, res) => {
-    res.status(404).json({
-        "message": 'not found'
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.status(200).json(users);
     })
-})
+    .catch((err) => {
+      res.status(500).json({
+        message: "eerr",
+        stack: err.stack,
+      });
+    });
+});
+//by id
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "eerr",
+        stack: err.stack,
+      });
+    });
+});
 
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
 
-module.exports = server; 
+module.exports = server;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
-console.log('ho')
+console.log('hello')
 
 server.listen(port, () => {
     console.log('listening on', port)
